a6c4b2c5ecf2ea8b7ae1ff4055301b1f3d7aecdd
FeralHorse <pusiket90@yandex.ru>
2024-03-14 20:10:40 +0300

    complete homework 4.2 corrected remarks

--- scripts423.sql
+++ scripts423.sql
@@ -1,3 +1,7 @@
 SELECT faculties.name, students.faculty_id AS id, students.name, students.age
 FROM faculties INNER JOIN students
-                      ON faculties.id = students.faculty_id
+                    ON faculties.id = students.faculty_id;
+SELECT avatars.filePath, avatars.student_id as id, avatars.fileSize, avatars.mediaType, avatars.data, students.name, students.age
+FROM avatars INNER JOIN students
+                    ON avatars.student_id = student.id WHERE avatars.filePath != NULL;
+
